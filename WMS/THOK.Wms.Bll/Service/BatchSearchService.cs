026b25961d8b93c8a47b0e08bdd35460b5609923
Dong-Yang <772369620@qq.com>
2014-11-28 11:53:50 +0800

    修改批次库存查询

--- WMS/THOK.Wms.Bll/Service/BatchSearchService.cs
+++ WMS/THOK.Wms.Bll/Service/BatchSearchService.cs
@@ -18,29 +18,15 @@ namespace THOK.Wms.Bll.Service
         {
             get { return this.GetType(); }
         }
-        [Dependency]
-        public IViewCmdProductRepository ViewCmdProductRepository { get; set; }
-        [Dependency]
-        public IViewWcsTaskRepository ViewWcsTaskRepository { get; set; }
 
         [Dependency]
         public IVIEWSTORAGERepository ViewstorageRepository { get; set; }
         [Dependency]
         public IViewbillmastRepository ViewbillmastRepository { get; set; }
         [Dependency]
-        public IWMSBillDetailHRepository BillDetailRepository { get; set; }
-        [Dependency]
         public ICMDCigaretteRepository CMDCigaretteRepository { get; set; }
         [Dependency]
         public IWMSFormulaMasterRepository FormulMasterRepository { get; set; }
-        [Dependency]
-        public IWMSFormulaDetailRepository FormulDetailRepository { get; set; }
-        [Dependency]
-        public ICMDCraneRepository CraneRepository { get; set; }
-        [Dependency]
-        public ICMDShelfRepository ShelfRepository { get; set; }
-        [Dependency]
-        public ICMDCellRepository CellRepository { get; set; }
 
         //批次库存查询
 
@@ -84,7 +70,7 @@ namespace THOK.Wms.Bll.Service
                                   f.FORMULA_NAME
                               };
 
-            bilLstorage = bilLstorage.OrderByDescending(i => i.BILL_DATE);
+            bilLstorage = bilLstorage.OrderByDescending(i => i.BILL_DATE).ThenByDescending(i=>i.BILL_NO);
             int total = bilLstorage.Count();
             bilLstorage = bilLstorage.Skip((page - 1) * rows).Take(rows);
             var temp = bilLstorage.ToArray().Select(i => new
@@ -144,7 +130,7 @@ namespace THOK.Wms.Bll.Service
             {
                 i.CELL_CODE,
                 IS_ACTIVE = i.IS_ACTIVE == "1" ? "可用" : "禁用",
-                IS_LOCK = i.IS_LOCK == "1" ? "可用" : "锁定",
+                IS_LOCK = i.IS_LOCK == "1" ? "锁定" : "可用",
                 i.PRODUCT_NAME,
                 i.PRODUCT_BARCODE,
                 i.GRADE_CODE,
@@ -161,67 +147,6 @@ namespace THOK.Wms.Bll.Service
             });
             return new { total, rows = temp };
         }
-
-        //public object GetSubDetails(int page, int rows, string BILL_NO)
-        //{
-        //    var storagequery = ViewstorageRepository.GetQueryable();
-        //    var billmast = ViewbillmastRepository.GetQueryable();
-        //    var cigaret = CMDCigaretteRepository.GetQueryable();
-        //    var formula = FormulMasterRepository.GetQueryable();
-        //    var billnos = from a in storagequery
-        //                  where a.BILL_NO == BILL_NO
-        //                  group a by new { a.CELL_CODE, a.PRODUCT_BARCODE, a.BILL_NO, a.GRADE_CODE, a.ORIGINAL_CODE, a.YEARS, a.IN_DATE } into g
-        //                  select new
-        //                  {
-        //                      g.Key,
-        //                      CELLSTATU = storagequery.FirstOrDefault(n => n.CELL_CODE == g.Key.CELL_CODE).IS_ACTIVE,
-        //                      TOTALPACKAGE = g.Count(),
-        //                      TOTALWEIGHT = g.Sum(i => i.REAL_WEIGHT),
-        //                      g.Key.IN_DATE
-        //                  };
-
-        //    var bilLstorage = from a in billmast
-        //                      join b in billnos on a.BILL_NO equals b.Key.BILL_NO
-        //                      join c in cigaret on a.CIGARETTE_CODE equals c.CIGARETTE_CODE
-        //                      join f in formula on a.FORMULA_CODE equals f.FORMULA_CODE
-        //                      select new
-        //                      {
-        //                          GRADE_CODE = b.Key.GRADE_CODE,
-        //                          b.Key.CELL_CODE,
-        //                          b.CELLSTATU,
-        //                          b.Key.PRODUCT_BARCODE,
-        //                          BILL_NO = b.Key.BILL_NO,
-        //                          a.BILL_DATE,
-        //                          a.CIGARETTE_CODE,
-        //                          c.CIGARETTE_NAME,
-        //                          a.FORMULA_CODE,
-        //                          f.FORMULA_NAME,
-        //                          b.TOTALPACKAGE,
-        //                          b.TOTALWEIGHT,
-        //                          b.IN_DATE
-        //                      };
-        //    bilLstorage = bilLstorage.OrderByDescending(i => i.IN_DATE);
-        //    int total = bilLstorage.Count();
-        //    bilLstorage = bilLstorage.Skip((page - 1) * rows).Take(rows);
-        //    var temp = bilLstorage.ToArray().Select(i => new
-        //    {
-        //        i.CELL_CODE,
-        //        CELLSTATU = i.CELLSTATU == "1" ? "可用" : "禁用",
-        //        i.PRODUCT_BARCODE,
-        //        i.GRADE_CODE,
-        //        BARCODEID = (i.PRODUCT_BARCODE).Substring(55, 10),//条码标示符
-        //        i.BILL_NO,
-        //        BILL_DATE = i.BILL_DATE.ToString("yyyy-MM-dd HH"),
-        //        IN_DATE = i.IN_DATE.ToString("yyyy-MM-dd HH:mm:ss"),
-        //        i.CIGARETTE_NAME,
-        //        i.FORMULA_NAME,
-        //        i.TOTALWEIGHT,
-        //        i.TOTALPACKAGE
-        //    });
-        //    return new { total, rows = temp };
-        //}
-
-
 
     }
 }
